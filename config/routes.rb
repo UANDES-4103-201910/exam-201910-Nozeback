95449003077e33e14d1f41a856876fca038f2715
alberto Altermatt <ajaltermatt@miuandes.cl>
2019-06-18 10:33:59 -0400

    anadiendo bootstrap y devise

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :users
   resources :products
   resources :order_statuses
+  root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
