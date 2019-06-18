4d3d0502352201ed568fb810431a74c859b39b4d
alberto Altermatt <ajaltermatt@miuandes.cl>
2019-06-18 12:34:13 -0400

    fallando por devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :products
   get 'home/index'
   resources :order_items
   resources :orders
   resources :addresses
-  resources :users
   resources :order_statuses
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
