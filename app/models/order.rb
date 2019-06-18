f667b577831ce7d3e5ee6e5e76424ea1cef61aee
alberto Altermatt <ajaltermatt@miuandes.cl>
2019-06-18 09:49:16 -0400

    dependencias creadas

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
-  belongs_to :user
-  belongs_to :orderstatus
+	has_many :order_items
+  	belongs_to :user
+  	belongs_to :orderstatus
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :order_items
   resources :orders
   resources :addresses
